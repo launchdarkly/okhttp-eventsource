cfed0c4552744e9d9339f6c3673bb0be4d358142
Phillip Wray <phillip.wray@robotsandpencils.com>
2017-10-04 09:20:59 -0600

    Issue #24 - trim indenting to 2-space tabs.

--- src/test/java/com/launchdarkly/eventsource/ManualTest.java
+++ src/test/java/com/launchdarkly/eventsource/ManualTest.java
@@ -10,38 +10,38 @@ import java.net.URI;
 
 @Ignore
 public class ManualTest {
-    private static final Logger logger = LoggerFactory.getLogger(ManualTest.class);
+  private static final Logger logger = LoggerFactory.getLogger(ManualTest.class);
 
-    @Test
-    public void manualTest() throws InterruptedException {
-        EventHandler handler = new EventHandler() {
-            public void onOpen() throws Exception {
-                logger.info("open");
-            }
+  @Test
+  public void manualTest() throws InterruptedException {
+    EventHandler handler = new EventHandler() {
+      public void onOpen() throws Exception {
+        logger.info("open");
+      }
 
-            @Override
-            public void onClosed() throws Exception {
+      @Override
+      public void onClosed() throws Exception {
 
-            }
+      }
 
-            public void onMessage(String event, MessageEvent messageEvent) throws Exception {
-                logger.info(event + ": " + messageEvent.getData());
-            }
+      public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        logger.info(event + ": " + messageEvent.getData());
+      }
 
-            public void onError(Throwable t) {
-                logger.error("Error: " + t);
-            }
+      public void onError(Throwable t) {
+        logger.error("Error: " + t);
+      }
 
-            public void onComment(String comment) {
-                logger.info("comment: " + comment);
-            }
-        };
-        EventSource source = new EventSource.Builder(handler, URI.create("http://localhost:8080/events/")).build();
-        source.start();
-        logger.warn("Sleeping...");
-        Thread.sleep(10000L);
-        logger.debug("Stopping source");
-        source.close();
-        logger.debug("Stopped");
-    }
+      public void onComment(String comment) {
+        logger.info("comment: " + comment);
+      }
+    };
+    EventSource source = new EventSource.Builder(handler, URI.create("http://localhost:8080/events/")).build();
+    source.start();
+    logger.warn("Sleeping...");
+    Thread.sleep(10000L);
+    logger.debug("Stopping source");
+    source.close();
+    logger.debug("Stopped");
+  }
 }

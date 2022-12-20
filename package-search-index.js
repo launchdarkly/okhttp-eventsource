9159311f1bca55a858449c0963289a1d0f6f15e5
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2022-12-20 01:04:15 +0000

    Updating documentation to version 4.0.0

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.launchdarkly.eventsource"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.launchdarkly.eventsource"},{"l":"com.launchdarkly.eventsource.background"}]

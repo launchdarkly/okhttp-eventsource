0919605dfb1daec4fc48d669a7eb86ed8fc14e98
Eli Bishop <eli@launchdarkly.com>
2022-12-30 19:45:15 -0800

    update Gradle to 7.6 + fix snapshot releases

--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -4,4 +4,9 @@ set -ue
 
 # Publish to Sonatype
 echo "Publishing to Sonatype"
-./gradlew publishToSonatype closeAndReleaseRepository || { echo "Gradle publish/release failed" >&2; exit 1; }
+if [[ -n "${LD_RELEASE_IS_PRERELEASE}" ]]; then
+  ./gradlew publishToSonatype || { echo "Gradle publish/release failed" >&2; exit 1; }	
+else
+  ./gradlew publishToSonatype closeAndReleaseRepository || { echo "Gradle publish/release failed" >&2; exit 1; }
+fi
+

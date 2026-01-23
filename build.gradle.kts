06b3c5f77af85ad21da37f0ed94f11bf96e84838
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2026-01-23 09:36:42 -0800

    chore: Only accept credentials via command line.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.external.javadoc.CoreJavadocOptions
 import org.gradle.external.javadoc.StandardJavadocDocletOptions
 
-// These values come from gradle.properties or command line
-val ossrhUsername: String? by project
-val ossrhPassword: String? by project
-
 buildscript {
     repositories {
         mavenCentral()
